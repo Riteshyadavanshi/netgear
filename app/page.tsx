9b04e84c1aec8bb2852d9c943868db6314ebe138
Riteshyadavanshi <riteshyadav4122@gmail.com>
2025-02-06 01:33:22 +0530

    secont

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 
-import { useState } from 'react';
-import Image from 'next/image';
+ 
 import { Navbar } from '@/components/Navbar';
 import { HeroSection } from '@/components/HeroSection';
 import { AboutUs } from '@/components/AboutUs';
@@ -8,19 +7,7 @@ import { WhyUs } from '@/components/WhyUs';
 import { ServiceSection } from '@/components/Service-Section';
 import { Contact } from '@/components/Contact';
 
-async function getData() {
-  const sheetId = '1nWbNT4fpJ-gSUQ3awi-C9YbnVld9aRpKJPploPqF6Xo';
-  const res = await fetch(
-    `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?tqx=out:json`
-    
-  );
-  
-  if (!res.ok) throw new Error('Failed to fetch data');
-  
-  const rawText = await res.text();
-  const jsonString = rawText.match(/google\.visualization\.Query\.setResponse\(([\s\S]+?)\);/)?.[1];
-  return jsonString ? JSON.parse(jsonString) : null;
-}
+
 
 export default async function Home() {
  
